77454f795e8297c13c5835925593f044121e8573
Vemburaj321 <113839911+Vemburaj321@users.noreply.github.com>
2023-05-01 23:25:29 +0530

    updated public ip of cloud in apislice.js

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseURI = 'http://localhost:8000';
+const baseURI = 'http://100.26.106.246:8000';
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({baseUrl: baseURI}),
